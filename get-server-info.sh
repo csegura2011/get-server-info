a201bc7c5fb16b40d21aa91411670c420b299d48
csegura2011 <cristian.segura.lepe@gmail.com>
2022-05-19 14:17:11 -0400

    Adding header comments to *sh script

--- get-server-info.sh
+++ get-server-info.sh
@@ -1,4 +1,17 @@
 #!/bin/bash 
+###############################################################################
+# File: get-server-info.sh                                                    #
+# Author: Cristian Segura <cristian.segura.lepe@gmail.com>                    #
+# Creation date: Tue May  3 16:11:44 UTC 2022                                 #
+# Usage: Get Linux Server Information                                         #
+#        (see README.lst for further information)                             #
+###############################################################################
+# History                                                                     #
+# =======                                                                     #
+# 2022.05.19: v0.1alpha Creation of this file (cristian segura)               #
+#                                                                             #
+###############################################################################
+
 
 
 printf "------------------------------------"
